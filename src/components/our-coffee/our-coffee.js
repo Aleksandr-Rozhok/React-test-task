af9e30f7d66dfd12f85ee5bcb4ccd1cb65fee53f
Aliaksandr Razhok <aliaksandr.razhok@LeverX.local>
2023-03-20 21:00:09 +0300

    add: finished Our Coffee page

--- src/components/our-coffee/our-coffee.js
+++ src/components/our-coffee/our-coffee.js
@@ -7,24 +7,26 @@ import DescriptionBlock from "../description-block/description-block";
 import "./our-coffee.css"
 import background from "../../img/our-coffee-header.jpg"
 import imgForDesc from "../../img/aromise.jpg"
+import BeanDivider from "../bean-divider/bean-divider";
 
 class OurCoffee extends Component {
 
     render() {
-        const firstTextPart = `Country: Brasil`;
-        const secondTextPart = `Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`;
-        
         return (
         <>
             <div className="our-coffee_container">
                 <Header title="Our Coffee" background={background} />
-                <div className="about-beans">
+                <div className="about-section">
                     <img src={imgForDesc} alt="girl" className="img-about-it" />
-                    <DescriptionBlock 
-                        width={"720"}
-                        title={"About it"}
-                        firstTextPart={firstTextPart} 
-                        secondTextPart={secondTextPart}/>
+                    <div className="about-it">
+                        <h3 className="desc-title">About it</h3>
+                        <BeanDivider />
+                        <div className="desc-container">
+                            <p className="desc-category">Country: <span className="text">Brasil</span></p>
+                            <p className="desc-category">Description: <span className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></p>
+                            <p className="desc-category">Price: <span className="desc-price">16.99$</span></p>
+                        </div>
+                    </div>
                 </div>
                 <Footer />
             </div>
